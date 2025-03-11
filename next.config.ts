5935bb0ffaeb2fbf742af3e6256c7883945c3432
nuzulfikrie <nuzulfikrie@gmail.com>
2025-03-11 23:26:33 +0800

    try fix for cf deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',  // This replaces the need for 'next export'
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
